345964c85b4bc10f939941a8842acec91c535241
priyakanchan <priya.kanchan13@gmail.com>
2022-06-23 18:28:36 +0530

    Modified imports

--- src/test/java/com/sandbox/bdd/Service/AddCustomerMockTest.java
+++ src/test/java/com/sandbox/bdd/Service/AddCustomerMockTest.java
@@ -1,6 +1,9 @@
 package com.sandbox.bdd.Service;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import com.braintreegateway.BraintreeGateway;
 import com.braintreegateway.Customer;
 import com.braintreegateway.CustomerRequest;
@@ -10,7 +13,6 @@ import com.sandbox.braintree.Service.InitializeService;
 import com.sandbox.braintree.Service.InitializeServiceImpl;
 import com.sandbox.braintree.model.AddInstrumentRequest;
 import com.sandbox.braintree.model.PaymentMethodResponse;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -18,11 +20,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
-
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 public class AddCustomerMockTest {
     @Mock
@@ -39,19 +36,21 @@ public class AddCustomerMockTest {
         MockitoAnnotations.initMocks(this);
 
     }
+
     @Test
     public void testAddCustomer() throws Exception {
-          BraintreeGateway braintreeGateway=new BraintreeGateway("sandbox","psbgxppgzh88j6tp","7snrhvgj4kbzwg5w","2668adf35fe5f3673eecdd2bf9bf621a");
-          final Result<Customer> addInstrumentRequestResult = customerResult();
-          when(initializeService.addCustomer(any(AddInstrumentRequest.class))).thenReturn(buildCustomerResponse());
-          when(braintreeGatewayConfig.getBraintreeGatewayInstance()).thenReturn(braintreeGateway);
-          PaymentMethodResponse paymentMethodResponse;
-          paymentMethodResponse = initializeServiceImpl.addCustomer(buildCustomerRequest());
-          assertEquals(200, Integer.parseInt(paymentMethodResponse.getCode()));
-      //    verify(initializeService,times(1)).addCustomer(refEq(buildCustomerRequest()));
-}
-    private  AddInstrumentRequest buildCustomerRequest() {
-        AddInstrumentRequest addInstrumentRequest= new AddInstrumentRequest();
+        BraintreeGateway braintreeGateway = new BraintreeGateway("sandbox", "psbgxppgzh88j6tp", "7snrhvgj4kbzwg5w", "2668adf35fe5f3673eecdd2bf9bf621a");
+        final Result<Customer> addInstrumentRequestResult = customerResult();
+        when(initializeService.addCustomer(any(AddInstrumentRequest.class))).thenReturn(buildCustomerResponse());
+        when(braintreeGatewayConfig.getBraintreeGatewayInstance()).thenReturn(braintreeGateway);
+        PaymentMethodResponse paymentMethodResponse;
+        paymentMethodResponse = initializeServiceImpl.addCustomer(buildCustomerRequest());
+        assertEquals(200, Integer.parseInt(paymentMethodResponse.getCode()));
+        //    verify(initializeService,times(1)).addCustomer(refEq(buildCustomerRequest()));
+    }
+
+    private AddInstrumentRequest buildCustomerRequest() {
+        AddInstrumentRequest addInstrumentRequest = new AddInstrumentRequest();
         addInstrumentRequest.setFirst_name("Neha");
         addInstrumentRequest.setLast_name("xyz");
         addInstrumentRequest.setCompany("Ticketmaster");
@@ -59,16 +58,18 @@ public class AddCustomerMockTest {
         addInstrumentRequest.setEmail("neha.sharma12@gmail.com");
         return addInstrumentRequest;
     }
-    private  PaymentMethodResponse buildCustomerResponse() {
-        PaymentMethodResponse paymentMethodResponse=new PaymentMethodResponse();
+
+    private PaymentMethodResponse buildCustomerResponse() {
+        PaymentMethodResponse paymentMethodResponse = new PaymentMethodResponse();
         paymentMethodResponse.setFirstname("Neha");
         paymentMethodResponse.setCode("200");
         paymentMethodResponse.setMessage("ok");
         return paymentMethodResponse;
     }
-    private  CustomerRequest buildRequest() {
-        CustomerRequest customerRequest=new CustomerRequest()
-        .firstName("Neha")
+
+    private CustomerRequest buildRequest() {
+        CustomerRequest customerRequest = new CustomerRequest()
+                .firstName("Neha")
                 .lastName("xyz")
                 .company("TCS")
                 .email("neha.sharma12@gmail.com")
@@ -83,5 +84,4 @@ public class AddCustomerMockTest {
         when(mockCustomerResult.getTarget()).thenReturn(mockCustomer);
         return mockCustomerResult;
     }
-
 }
